e1d488982abc8b8756afeb31dd5ebfceb2376254
Denys Fedoryshchenko <denys.f@collabora.com>
2025-05-04 00:19:28 +0300

    Reformatting and few additional functions

--- felicity_ivem.py
+++ felicity_ivem.py
@@ -9,6 +9,7 @@ from pymodbus.client.serial import ModbusSerialClient
 from pymodbus.constants import Endian
 from pymodbus.payload import BinaryPayloadDecoder
 import logging
+import time
 
 PORT_NAME = "/dev/ttyUSB0"  # Change this to your serial port
 
@@ -19,13 +20,9 @@ log.setLevel(logging.INFO)
 
 # Modbus RTU client configuration
 client = ModbusSerialClient(
-    port=PORT_NAME,
-    baudrate=2400,
-    stopbits=1,
-    bytesize=8,
-    parity='N',
-    timeout=4
+    port=PORT_NAME, baudrate=2400, stopbits=1, bytesize=8, parity="N", timeout=4
 )
+
 
 class Inverter:
     register_map = {
@@ -44,7 +41,7 @@ class Inverter:
         "load_percentage": 0x1120,
         "pv_input_voltage": 0x1126,
         "pv_input_power": 0x112A,
-        "battery_percentage": 0x1132
+        "battery_percentage": 0x1132,
     }
 
     def __init__(self, client):
@@ -57,15 +54,29 @@ class Inverter:
         self.client.close()
 
     def read_holding_registers(self, address, count=1, slaveid=1):
-        return self.client.read_holding_registers(address=address, count=count, slave=slaveid)
+        return self.client.read_holding_registers(
+            address=address, count=count, slave=slaveid
+        )
+
+    def read_register_raw(self, address):
+        """Retry on failure 3 times"""
+        for i in range(3):
+            result = self.client.read_holding_registers(address=address)
+            if not result.isError():
+                return result
+            else:
+                log.error(
+                    f"Error reading register {address}: {result}, retrying {i+1}/3"
+                )
+        return None
 
     def read_register(self, name):
-        #print(f"Reading register: {name}")
+        # print(f"Reading register: {name}")
         convert_types = {
             "battery_power": self.client.DATATYPE.INT16,
             "battery_current": self.client.DATATYPE.INT16,
             "ac_output_power": self.client.DATATYPE.INT16,
-            "ac_output_apparent_power": self.client.DATATYPE.INT16
+            "ac_output_apparent_power": self.client.DATATYPE.INT16,
         }
         if name in self.register_map:
             address = self.register_map[name]
@@ -74,7 +85,8 @@ class Inverter:
                 data_type = convert_types[name]
             else:
                 data_type = self.client.DATATYPE.UINT16
-            result = self.read_holding_registers(address, count)
+            # Read the register
+            result = self.read_register_raw(address)
             if not result.isError():
                 val = self.client.convert_from_registers(result.registers, data_type)
                 # normalize the value
@@ -88,7 +100,6 @@ class Inverter:
                 log.error(f"Error reading register {name}: {result}")
                 return None
 
-    
     # List registers and associate with normalized names
     def get_register_by_name(self, name):
         return self.register_map.get(name)
@@ -110,7 +121,7 @@ class Inverter:
                     3: "BatteryMode",
                     4: "FaultMode",
                     5: "LineMode",
-                    6: "PVChargeMode"
+                    6: "PVChargeMode",
                 }
                 return modes.get(value, "Unknown")
             elif register == "charging_state":
@@ -118,13 +129,13 @@ class Inverter:
                     0: "NoCharge",
                     1: "ConstantCurrent",
                     2: "ConstantVoltage",
-                    3: "Float"
+                    3: "Float",
                 }
                 return states.get(value, "Unknown")
             elif register == "model":
                 models = {
-                    0x0408: "IVEM5048-(5000VA/48V)",
-                    0x0204: "IVEM3024-(3000VA/24V)"
+                    0x0408: "IVEM5048(5000VA/48V)",
+                    0x0204: "IVEM3024(3000VA/24V)",
                 }
                 return models.get(value, "Unknown")
             else:
@@ -132,7 +143,7 @@ class Inverter:
         else:
             log.error(f"Register {register} not found in register map.")
             return None
-    
+
     def read_all_registers(self):
         all_registers = {}
         for name in self.register_map:
@@ -143,21 +154,98 @@ class Inverter:
                 log.error(f"Failed to read register {name}")
         return all_registers
 
+    def human_time(self, seconds):
+        """Convert seconds to human-readable format HH:MM:SS"""
+        hours = int(seconds // 3600)
+        minutes = int((seconds % 3600) // 60)
+        seconds = int(seconds % 60)
+        return f"{hours:02}:{minutes:02}:{seconds:02}"
+
+    def estimate_battery_runtime(self):
+        """Estimate battery runtime based on battery percentage and average load(power in W)
+        Read until percentage flips to lower level, record timestamp, start recording power values each 10 seconds
+        when next percentage is lower than previous, calculate average power vs time elapsed
+        """
+        current_percentage = self.read_register("battery_percentage")
+        if current_percentage is None:
+            log.error("Failed to read battery percentage")
+            return None
+        # Read until percentage flips to lower level
+        print("Waiting for battery percentage to drop...")
+        while True:
+            new_percentage = self.read_register("battery_percentage")
+            if new_percentage is None:
+                log.error("Failed to read battery percentage")
+                return None
+            if new_percentage < current_percentage:
+                current_percentage = new_percentage
+                break
+            current_percentage = new_percentage
+        # Record timestamp
+        print("Starting battery runtime estimation... ")
+        print(f"Current battery percentage: {current_percentage}%")
+        start_time = time.time()
+
+        # Record power values each 10 seconds
+        power_values = []
+        while True:
+            power = self.read_register("battery_power")
+            if power is None:
+                log.error("Failed to read battery power")
+                return None
+            power_values.append(power)
+            print(".", end="")
+            new_percentage = self.read_register("battery_percentage")
+            if new_percentage is None:
+                log.error("Failed to read battery percentage")
+                return None
+            if new_percentage < current_percentage:
+                break
+            time.sleep(5)
+
+        print("\nBattery percentage dropped to:", new_percentage)
+        # Calculate average power
+        average_power = sum(power_values) / len(power_values)
+        # Calculate elapsed time
+        elapsed_time = time.time() - start_time
+        print(
+            f"Average power: {average_power} W consumes 1% of battery in {self.human_time(elapsed_time)}"
+        )
+        # Calculate estimated runtime (to 20% of battery)
+        estimated_runtime = (current_percentage - 20) * elapsed_time
+        print(f"Estimated runtime: {self.human_time(estimated_runtime)}")
+        return estimated_runtime
+
+
 def main():
     inverter = Inverter(client)
     if inverter.connect():
         log.info("Connected to inverter")
+        estimate_battery_runtime = inverter.estimate_battery_runtime()
+        if estimate_battery_runtime is not None:
+            log.info(f"Estimated battery runtime: {estimate_battery_runtime}")
+        else:
+            log.error("Failed to estimate battery runtime")
         try:
             # Read all registers
             all_registers = inverter.read_all_registers()
             for name, value in all_registers.items():
                 log.info(f"{name}: {value}")
+            # scan from 0x1100 to 0x112F
+            # for i in range(0x112F, 0x11FF):
+            #    result = inverter.read_holding_registers(i, 1)
+            #    if not result.isError():
+            #        val = inverter.client.convert_from_registers(result.registers, inverter.client.DATATYPE.UINT16)
+            #        log.info(f"Register {hex(i)}: {val} {hex(val)}")
+            #    else:
+            #        log.error(f"Error reading register {i}: {result}")
         except Exception as e:
             log.error(f"Error reading registers: {e}")
         finally:
             inverter.close()
     else:
         log.error("Failed to connect to inverter")
-        
+
+
 if __name__ == "__main__":
     main()
